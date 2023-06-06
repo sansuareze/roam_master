572644f6509d4cc8c07f5ba9c19d691327f57250
Friedrich von Grone <fitti.grone@westerbrak.de>
2023-06-06 12:28:55 +0200

    pundit

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, post)
+    @user = user
+    @post = post
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    user.admin? || !post.published?
+  end
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      raise NotImplementedError, "You must define #resolve in #{self.class}"
+    end
+
+    private
+
+    attr_reader :user, :scope
+  end
+end
