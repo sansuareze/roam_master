8c4c8a2d2874b314b6e9f69c089dafa3091b1ca6
Friedrich von Grone <fitti.grone@westerbrak.de>
2023-06-13 10:07:43 +0200

    number of days

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -16,6 +16,24 @@ class TripsController < ApplicationController
   def create
     @trip = Trip.new(trip_params)
     @trip.user = current_user
+    start_date = DateTime.new(
+      params[:trip]["start_date(1i)"].to_i,
+      params[:trip]["start_date(2i)"].to_i,
+      params[:trip]["start_date(3i)"].to_i,
+      params[:trip]["start_date(4i)"].to_i,
+      params[:trip]["start_date(5i)"].to_i
+    )
+
+    end_date = DateTime.new(
+      params[:trip]["end_date(1i)"].to_i,
+      params[:trip]["end_date(2i)"].to_i,
+      params[:trip]["end_date(3i)"].to_i,
+      params[:trip]["end_date(4i)"].to_i,
+      params[:trip]["end_date(5i)"].to_i
+    )
+
+    @trip.number_of_days = (end_date.to_date - start_date.to_date).to_i
+
     @trip.save
     authorize @trip
     redirect_to trip_path(@trip)
@@ -24,7 +42,6 @@ class TripsController < ApplicationController
   def edit
     @trip = Trip.find(params[:id])
   end
-
 
   def update
     @trip = Trip.find(params[:id])
@@ -37,6 +54,7 @@ class TripsController < ApplicationController
     @trip.destroy
     redirect_to restaurants_path, status: :see_other
   end
+
 
   private
 
