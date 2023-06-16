7ff6dc5012535e8e0873a5fc987f5845577bbce1
Santiago Suarez <sansuareze@gmail.com>
2023-06-16 15:58:44 +0200

    changed api keys

--- app/javascript/controllers/search_api_controller.js
+++ app/javascript/controllers/search_api_controller.js
@@ -46,8 +46,8 @@ export default class extends Controller {
     const url = 'https://test.api.amadeus.com/v1/security/oauth2/token';
     const data = {
       grant_type: "client_credentials",
-      client_id: '9ulXYrcAbq4ApwJ2kO3rab85s05Sk5OL',
-      client_secret: 'GyG2eGE7X6BEmUIO',
+      client_id: 'Nd23iTdbMSx49DNUNSY8nA4vXfPeHWkl',
+      client_secret: 'zHZhhCyoHxCra3Vz',
     }
 
     const body = new URLSearchParams();
